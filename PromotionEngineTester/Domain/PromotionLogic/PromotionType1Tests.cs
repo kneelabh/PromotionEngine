56439860e037db6b5d55e0bc29bc38f695229070
Kumar Neelabh <krneelabh@gmail.com>
2020-06-12 23:15:51 +0530

    promotion 1 test cases

--- PromotionEngineTester/Domain/PromotionLogic/PromotionType1Tests.cs
+++ PromotionEngineTester/Domain/PromotionLogic/PromotionType1Tests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PromotionEngine.Domain.PromotionLogic;
+using PromotionEngineTester.Domain.PromotionLogic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,37 @@ namespace PromotionEngine.Domain.PromotionLogic.Tests
         [TestMethod()]
         public void ParseTest()
         {
+            float sum = 130;
+            DummyCart dummyCart = new DummyCart();
+            var listItems = dummyCart.Generate();
+            PromotionType1 PromotionType1 = new PromotionType1();
+            var cost = PromotionType1.Parse(listItems);
 
+            Assert.AreEqual(sum, cost);
+            Assert.AreEqual(3, listItems.Count());
+        }
+
+        [TestMethod()]
+        public void ParseTestEmptyList()
+        {
+            float sum = 0;
+            PromotionType1 PromotionType1 = new PromotionType1();
+            var cost = PromotionType1.Parse(new List<SKUItems>());
+
+            Assert.AreEqual(sum, cost);
+        }
+
+        [TestMethod()]
+        public void ParseTestListVar2()
+        {
+            float sum = 130;
+            DummyCart dummyCart = new DummyCart();
+            var listItems = dummyCart.Generate0Promotion3();
+            PromotionType1 PromotionType1 = new PromotionType1();
+            var cost = PromotionType1.Parse(listItems);
+
+            Assert.AreEqual(sum, cost);
+            Assert.AreEqual(1, listItems.Count());
         }
     }
 }
