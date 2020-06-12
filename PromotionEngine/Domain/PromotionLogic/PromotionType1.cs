60edf37d80731baaf52c086aae95e5d9e209c17a
Kumar Neelabh <krneelabh@gmail.com>
2020-06-12 23:16:06 +0530

    Promotion1 code

---
+++ PromotionEngine/Domain/PromotionLogic/PromotionType1.cs
@@ -0,0 +1,35 @@
+﻿using PromotionEngine.Domain.SKU;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Domain.PromotionLogic
+{
+    public class PromotionType1 : IPromotionLogic
+    {
+        int countOfSKU;
+        float cost;
+        ISKU offerSKU;
+        public float Parse(List<SKUItems> listSKU)
+        {
+            float totalCost = 0;
+            var eligibleItems = listSKU.Where(t => t.sKU.GetType() == offerSKU.GetType()).FirstOrDefault();
+
+            if (eligibleItems != null)
+            {
+                listSKU.Remove(eligibleItems);
+                totalCost = (eligibleItems.count / countOfSKU) * cost;
+            }
+            return totalCost;
+        }
+
+        public PromotionType1()
+        {
+            offerSKU = new SKU_A();
+            cost = 130;
+            countOfSKU = 3;
+        }
+    }
+}
